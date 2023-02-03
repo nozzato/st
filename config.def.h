83d3a6ed3751e465eb25f09afe6d08ff0266628e
Noah Torrance <noahtorrance27@gmail.com>
2023-02-03 11:33:35 +0000

    Add emoji support

--- config.def.h
+++ config.def.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "DejaVuSansMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    "Blobmoji:pixelsize=12:antialias=true:autohint=true"
+};
+
 static int borderpx = 2;
 
 /*
